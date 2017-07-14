29cb052aeed96bec6dc29f5b10523a183c4a0566
wty19930106 <342566183@qq.com>
2017-07-14 12:08:25 +0800

    END2

--- 1.js
+++ 1.js
@@ -1,6 +1,6 @@
 
 var box=document.getElementById('box');
-box.addEventListener('mousedown'function(ev){
+box.addEventListener('mousedown',function(ev){
 	var disX = ev.clientX-this.offsetLeft;
 	var disY = ev.clientY-this.offsetTop;
 })
