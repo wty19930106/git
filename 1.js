6626dc3a35a872302973fbca962c3fe1ddc51cae
wty19930106 <342566183@qq.com>
2017-07-14 12:09:58 +0800

    end

--- 1.js
+++ 1.js
@@ -3,17 +3,19 @@ var box=document.getElementById('box');
 box.addEventListener('mousedown',function(ev){
 	var disX = ev.clientX-this.offsetLeft;
 	var disY = ev.clientY-this.offsetTop;
+	document.addEventListener('mousemove',fn);
+	document.addEventListener('mouseup',up);
+	function fn(ev){
+		var l=ev.clientX-disX;
+		var h=ev.clientY-disY;
+		box.style.left=l+'px';
+		box.style.top=h+'px';
+	}
+	function up(){
+		document.removeEventListener('mousemove',fn);
+		document.removeEventListener('mouseup',up);
+	}
 })
-document.addEventListener('mousemove',fn);
-document.addEventListener('mouseup',up);
-function fn(ev){
-	var l=ev.clientX-disX;
-	var h=ev.clientY-disY;
-	box.style.left=l+'px';
-	box.style.top=h+'px';
-}
-function up(){
-	document.removeEventListener('mousemove',fn);
-	document.removeEventListener('mouseup',up);
-}
+
+
 
